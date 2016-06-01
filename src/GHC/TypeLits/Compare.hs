d94a3de2942489de8a9dff9e838986603dd65994
Justin Le <justin@jle.im>
2016-06-01 15:32:45 -0700

    Compare module

---
+++ src/GHC/TypeLits/Compare.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+
+module GHC.TypeLits.Compare where
+
+import Data.Type.Equality
+import Data.Proxy
+import GHC.TypeLits
+import Unsafe.Coerce
+
+isLE
+    :: (KnownNat m, KnownNat n)
+    => Proxy m
+    -> Proxy n
+    -> Maybe ((m <=? n) :~: 'True)
+isLE m n = case m %<= n of
+             Right Refl -> Just Refl
+             Left _     -> Nothing
+
+isNLE
+    :: (KnownNat m, KnownNat n)
+    => Proxy m
+    -> Proxy n
+    -> Maybe ((m <=? n) :~: 'False)
+isNLE m n = case m %<= n of
+              Left Refl -> Just Refl
+              Right _   -> Nothing
+
+(%<=)
+    :: (KnownNat m, KnownNat n)
+    => Proxy m
+    -> Proxy n
+    -> Either ((m <=? n) :~: 'False) ((m <=? n) :~: 'True)
+m %<= n | natVal m <= natVal n = Right (unsafeCoerce Refl)
+        | otherwise            = Left (unsafeCoerce Refl)
+
